b939dd090ef7c7e550ae7af7cdb317173b26ffdc
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-10-16 15:56:05 -0200

    Created the index page

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./config/express')
-const port = 8086
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-  console.log('To down the server: ctrl + c')
-})
