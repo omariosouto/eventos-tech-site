22aea99cae3d8d918a0e78e05a9841f9d46bf00b
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-13 18:39:12 -0200

    Created the profile page

--- src/config/express.js
+++ src/config/express.js
@@ -3,6 +3,7 @@ const app = express()
 const nunjucks = require('nunjucks')
 
 const homeRouter = require('../routers/home')
+const perfilRouter = require('../routers/perfil')
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -12,5 +13,6 @@ nunjucks.configure('views', {
 app.use(express.static('public'))
 
 app.use('/', homeRouter)
+app.use('/perfil', perfilRouter)
 
 module.exports = app
