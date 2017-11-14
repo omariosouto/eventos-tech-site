c8dd4ff801630b91b61d68d956d16a3abbcecf3d
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-14 15:17:11 -0200

    Created route to /event/day

--- src/routers/event.js
+++ src/routers/event.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 router.get('/', (req, res) => res.render('event.html'))
 router.get('/category', (req, res) => res.render('event-category.html'))
+router.get('/day', (req, res) => res.render('event-day.html'))
 
-module.exports = router 
+module.exports = router
