c0c13282b430eef3e3cbcf2421683bce30c6a6d9
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-14 16:49:53 -0200

    Altered the menu include to menu macro

--- src/routers/event.js
+++ src/routers/event.js
@@ -4,5 +4,6 @@ const router = express.Router()
 router.get('/', (req, res) => res.render('event.html'))
 router.get('/category', (req, res) => res.render('event-category.html'))
 router.get('/day', (req, res) => res.render('event-day.html'))
+router.get('/like', (req, res) => res.render('event-like.html'))
 
 module.exports = router
