eb6107e6a1091bf9bf511c799ee0a120a18f1dde
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-14 14:40:38 -0200

    Created the route /event/category

--- src/routers/event.js
+++ src/routers/event.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => res.render('event.html'))
+router.get('/category', (req, res) => res.render('event-category.html'))
 
 module.exports = router 
