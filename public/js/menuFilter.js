27aeec403815f91ca68af1f8de6192e9ecdf6624
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-21 17:37:17 -0200

    Bigfix - action in menuFilter

--- public/js/menuFilter.js
+++ public/js/menuFilter.js
@@ -5,6 +5,10 @@
   const $wrapHeaderFilter = doc.querySelector('.wrap__header__filter')
   const $wrapMenuFilter = doc.querySelector('#menuFilter')
 
+  $wrapMenuFilter.addEventListener('click', (event) => {
+    event.cancelBubble = true
+  })
+
   $wrapHeaderFilter.addEventListener('click', (event) => {
     event.preventDefault()
 
@@ -14,9 +18,8 @@
   $body.addEventListener('click', (event) => {
     const $target = event.target
 
-    console.log($target)
-
-    if(!$target.classList.contains('wrap__menuFilter') && 
+    if($wrapMenuFilter.classList.contains('wrap__menuFilter--isActive') &&
+    !$target.classList.contains('wrap__menuFilter') && 
     !$target.classList.contains('wrap__header__filter') &&
     !$target.classList.contains('header__filter')) {
       $wrapMenuFilter.classList.toggle('wrap__menuFilter--isActive')
