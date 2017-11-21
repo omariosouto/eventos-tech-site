192c66aeeadb86a3c3378a3a6e8969726e30ac92
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-21 17:25:31 -0200

    Action in menuFilter

--- public/js/menuFilter.js
+++ public/js/menuFilter.js
@@ -1,5 +1,25 @@
-(function() {
+(function(doc) {
   'use strict'
 
-  
-})()
+  const $body = doc.querySelector('body')
+  const $wrapHeaderFilter = doc.querySelector('.wrap__header__filter')
+  const $wrapMenuFilter = doc.querySelector('#menuFilter')
+
+  $wrapHeaderFilter.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    $wrapMenuFilter.classList.toggle('wrap__menuFilter--isActive')
+  })
+
+  $body.addEventListener('click', (event) => {
+    const $target = event.target
+
+    console.log($target)
+
+    if(!$target.classList.contains('wrap__menuFilter') && 
+    !$target.classList.contains('wrap__header__filter') &&
+    !$target.classList.contains('header__filter')) {
+      $wrapMenuFilter.classList.toggle('wrap__menuFilter--isActive')
+    }
+  })
+})(document)
